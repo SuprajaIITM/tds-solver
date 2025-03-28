3cd1b9d9c21ea16ec697ca466c74fa87235704fb
SuprajaIITM <24f1002771@ds.study.iitm.ac.in>
2025-03-28 22:00:15 +0530

    Silly code mistake

--- model/solver.py
+++ model/solver.py
@@ -3,6 +3,5 @@ import pandas as pd
 import os
 
 def solve_question(question: str, file_path: str = None) -> str:
-    def solve_question(question: str, file_path: str = None) -> str:
     return "123456"  # Placeholder for testing
 
