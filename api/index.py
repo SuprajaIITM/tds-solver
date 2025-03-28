91d7104ba82590721cb16b3e41f6ddb5e2f21af7
SuprajaIITM <24f1002771@ds.study.iitm.ac.in>
2025-03-28 23:31:13 +0530

    Totally new

--- api/index.py
+++ api/index.py
@@ -4,11 +4,7 @@ from mangum import Mangum
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI on Vercel!"}
-
-@app.get("/greet")
-def greet(name: str = "World"):
-    return {"greeting": f"Hello, {name}!"}
+def home():
+    return {"message": "Hello from FastAPI on Vercel"}
 
 handler = Mangum(app)
